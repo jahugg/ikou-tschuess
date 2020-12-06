c0694e91f96a3fbc81b918ef9ef0637bdafa572d
Jan <jan@Some-New-MacBook-Pro.local>
2020-12-07 00:08:46 +0100

    added toggles and readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # ikou-tschuess
-shopping window pojection installation
+Shopping Window Pojection Installation for IKOU TSCHUESS
+
+# notes
+- blobs are initialized via javascript
+- blob animations are controled via css animations
+- projection cropping is set via css custom properties
+- animations use random values between 30 and 60s
+- every 3rd blob is colored white
+
+# helpful keyboard controls for setup
+use the following keyboard controls for setup
+
+toggle tile mask: *m*
+toggle cropping frame: *n*
+toggle white background: *b*
+
+# instructions for setup
+git clone ...
+yarn start
--- src/index.js
+++ src/index.js
@@ -1,5 +1,69 @@
+const blobCount = 10;
+
+console.info('toggle Mask: m\ntoggle Frame: n\ntoggle Background: b');
+
 // print mouse position
-let lamp = document.querySelector('body');
-lamp.addEventListener('click', (e) => {
-  console.log(e.clientX, e.clientY);
+document.addEventListener('click', (e) => {
+  console.info(`X:${e.clientX} Y:${e.clientY}`);
 });
+
+// toggle controls for setup
+document.addEventListener('keydown', (e) => {
+  const key = e.key.toLowerCase();
+
+  // toggle mask
+  if (key === 'm') {
+    // get current clip-path value
+    let el = document.getElementById('frame');
+    let value = window.getComputedStyle(el).clipPath;
+
+    if (value != 'none') el.style.clipPath = 'none';
+    else el.style.clipPath = 'url(#mask)';
+  }
+
+  // toggle frame
+  if (key === 'n') {
+    // get current border value
+    let el = document.getElementById('frame');
+    let value = window.getComputedStyle(el).border;
+
+    let defaultValue = '2px solid rgb(255, 0, 0)';
+    if (value != defaultValue) el.style.border = defaultValue;
+    else el.style.removeProperty('border');
+  }
+
+  // toggle frame
+  if (key === 'b') {
+    // get current border value
+    let el = document.getElementById('filter');
+    let value = window.getComputedStyle(el).background;
+
+    let defaultValue = 'rgb(0, 0, 0) none repeat scroll 0% 0% / auto padding-box border-box';
+    if (value != defaultValue) mel.style.background = "black";
+    else el.style.background = "white";
+  }
+});
+
+function initBlobs() {
+  let filter = document.getElementById('filter');
+
+  // create blobs with random animations
+  for (let i = 0; i < blobCount - 1; i++) {
+    let blob = document.createElement('div');
+    blob.classList.add('blob');
+    blob.style.animation = `updown ease-in ${getRandomInt(30, 60)}s infinite alternate,
+    leftright ease-in-out ${getRandomInt(30, 60)}s infinite,
+    vastag linear ${getRandomInt(30, 60)}s infinite,
+    spin linear ${getRandomInt(30, 60)}s infinite`;
+    filter.appendChild(blob);
+  }
+}
+
+initBlobs();
+
+// helper function
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
